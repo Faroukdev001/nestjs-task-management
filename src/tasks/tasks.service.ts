f85247f5784f33218b1daca1f1a36495ff28d5f4
Faroukdev001 <faroukarogundade@gmail.com>
2025-03-17 10:23:07 +0100

    feat: add getTasksById method and update createTask to use CreateTaskDto

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,16 +11,24 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title: string, description: string): Task {
+    getTasksById(id: string): Task {
+        const task = this.tasks.find((task) => task.id === id);
+        if (!task) {
+            throw new Error(`Task with ID ${id} not found`);
+        }
+        return task;
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+
         const task: Task = {
             id: uuid(),
             title,
             description,
             status: TaskStatus.OPEN,
         };
-        
         this.tasks.push(task);
         return task;
-
     }
 }
