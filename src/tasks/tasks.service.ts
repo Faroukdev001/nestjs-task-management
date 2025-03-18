853f930d583b742c9e80695bd58887ce1df0414f
Faroukdev001 <faroukarogundade@gmail.com>
2025-03-18 22:55:54 +0100

    feat: implement task filtering and add delete and update task status methods

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,10 +12,22 @@ export class TasksService {
         return this.tasks;
     }
 
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+        let tasks = this.getAllTasks();
+        if (status) {
+            tasks = tasks.filter((task) => task.status === status);
+        }
+        if (search) {
+            tasks = tasks.filter((task) => task.title.includes(search) || task.description.includes(search));
+        }
+        return tasks;
+    }
+
     getTasksById(id: string): Task {
         const task = this.tasks.find((task) => task.id === id);
         if (!task) {
-            throw new Error(`Task with ID ${id} not found`);
+            throw new NotFoundException(`Task with ID ${id} not found`);
         }
         return task;
     }
@@ -29,6 +42,18 @@ export class TasksService {
             status: TaskStatus.OPEN,
         };
         this.tasks.push(task);
+        return task;
+    }
+
+    deleteTask(id: string): void {
+        const found = this.getTasksById(id);
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+        console.log("Task deleted");
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const task = this.getTasksById(id);
+        task.status = status;
         return task;
     }
 }
