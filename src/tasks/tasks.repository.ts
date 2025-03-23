364f003457fb92d28e8c01804c3b595d0a4e8602
Faroukdev001 <faroukarogundade@gmail.com>
2025-03-23 21:47:15 +0100

    ``` feat: add functionality to enhance user authentication experience ```

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -4,6 +4,7 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { TaskStatus } from "./task-status.enum";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
+import { User } from "src/auth/user.entity";
 
 
 @Injectable()
@@ -12,9 +13,10 @@ export class TasksRepository extends Repository<Task> {
         super(Task, datasource.createEntityManager());
     }
 
-    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
         const { status, search } = filterDto;
         const query = this.createQueryBuilder('task');
+        query.where( { user } );
 
         if (status) {
             query.andWhere('task.status = :status', { status });
@@ -22,7 +24,7 @@ export class TasksRepository extends Repository<Task> {
 
         if (search) {
             query.andWhere(
-                'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+                '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
                 { search: `%${search}%` },
             );
         }
@@ -31,20 +33,21 @@ export class TasksRepository extends Repository<Task> {
         return tasks; 
     }
 
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
         const { title, description } = createTaskDto; // destructuring
 
         const task = this.create({
             title,
             description,
             status: TaskStatus.OPEN,
+            user,
         });
         await this.save(task);
         return task;
     }
 
-    async deleteTask(id: string): Promise<void> {
-        const result = await this.delete(id);
+    async deleteTask(id: string, user: User): Promise<void> {
+        const result = await this.delete({ id, user });
         if (result.affected === 0) {
             throw new NotFoundException(`Task with ID ${id} not found`);
         }
