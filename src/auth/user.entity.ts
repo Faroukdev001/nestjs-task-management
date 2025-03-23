5ad932fb245ed48772007550cc7cf84be6ea2522
Faroukdev001 <faroukarogundade@gmail.com>
2025-03-23 21:51:50 +0100

    feat: add one-to-many relationship between User and Task entities

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Task } from "src/tasks/task.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
@@ -6,9 +7,12 @@ export class User {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @Column()
+    @Column({ unique: true })
     username: string;
 
     @Column()
     password: string;
+
+    @OneToMany((_type) => Task, task => task.user, { eager: true})
+    tasks: Task[];
 }
