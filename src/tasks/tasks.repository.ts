54769ea79c16ea4cd34741256039893207cccfac
Faroukdev001 <faroukarogundade@gmail.com>
2025-03-24 15:04:43 +0100

    feat: add error handling and logging to task retrieval in TasksRepository

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,14 +1,16 @@
 import { DataSource, Repository } from "typeorm";
 import { Task } from "./task.entity";
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import { TaskStatus } from "./task-status.enum";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 import { User } from "src/auth/user.entity";
+import { Logger } from "@nestjs/common";
 
 
 @Injectable()
 export class TasksRepository extends Repository<Task> {
+    private logger = new Logger('TasksRepository', { timestamp: true });
     constructor(datasource: DataSource) {
         super(Task, datasource.createEntityManager());
     }
@@ -29,8 +31,15 @@ export class TasksRepository extends Repository<Task> {
             );
         }
 
-        const tasks = await query.getMany();
-        return tasks; 
+        try {
+            const tasks = await query.getMany();
+            return tasks; 
+        } catch (error) {
+            this.logger.error(`Failed to get tasks for user "${user.username}". Filters: ${JSON.stringify(filterDto)}`, error.stack);
+            throw new InternalServerErrorException();
+            
+        }
+
     }
 
     async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
