2bd78c634605fdc3cfd39053ed161a2c9fba698c
Faroukdev001 <faroukarogundade@gmail.com>
2025-03-24 15:05:26 +0100

    feat: use ConfigService for JWT secret management in JwtStrategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,14 +4,20 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { UsersRepository } from "./users.repository";
 import { JwtPayload } from "./jwt-payload.interface";
 import { User } from "./user.entity";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(
         private userRepository: UsersRepository,
+        private configService: ConfigService,
     ) {
+        const jwtSecret = configService.get<string>('JWT_SECRET');
+        if (!jwtSecret) {
+            throw new Error('JWT_SECRET is not defined in the configuration');
+        }
         super({
-            secretOrKey: 'topSecret51',
+            secretOrKey: jwtSecret,
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
         });
     }
